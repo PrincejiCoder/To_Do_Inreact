128b11523959d3082eb7748b242e7654209eb391
Prince Chauhan <youremail@example.com>
2024-10-24 23:59:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoList from './Todo'; // Adjust the path based on your file structure
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Render the TodoList component */}
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
